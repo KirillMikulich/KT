c2d7ee8a954f1aa74524144fa7ea81c115afeb13
KirillMikulich <kirill.mikulich@mail.ru>
2022-03-22 17:26:22 +0300

    Beaufort Cipher

---
+++ Ciphers/PZ2/BeaufortCipher.cs
@@ -0,0 +1,126 @@
+﻿using Ciphers.Constants;
+using System;
+
+namespace Ciphers.PZ2
+{
+    public static class BeaufortCipher
+    {
+        public static string Encrypt(string text, string key)
+        {
+            string cipherText = null;
+
+            text = text.ToUpper();
+            key = key.ToUpper();
+
+            if (!string.IsNullOrEmpty(text) && !string.IsNullOrEmpty(key))
+            {
+                for (int i = 0, j = 0; i < text.Length; i++)
+                {
+                    if (text[i] == ' ') continue;
+                    if (j >= key.Length) j = 0;
+
+                    
+                    int indexWord = FindChar(text[i].ToString());
+                    string[] row = GetColumn(indexWord);
+
+                    int indexKey = Array.IndexOf(row, key[j].ToString());
+
+                    if (indexKey != -1)
+                    {
+                        cipherText += VigenereAlphabet.AlphabetEn[indexKey];
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid string");
+                        Environment.Exit(0);
+                    }
+                    j++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid string");
+                Environment.Exit(0);
+            }
+
+
+            return cipherText;
+        }
+
+
+        public static string Decrypt(string text, string key)
+        {
+            string cipherText = null;
+
+            text = text.ToUpper();
+            key = key.ToUpper();
+
+            if (!string.IsNullOrEmpty(text) && !string.IsNullOrEmpty(key))
+            {
+                for (int i = 0, j = 0; i < text.Length; i++)
+                {
+                    if (text[i] == ' ') continue;
+                    if (j >= key.Length) j = 0;
+
+
+                    int indexWord = FindChar(text[i].ToString());
+                    string[] row = GetRow(indexWord);
+
+                    int indexKey = Array.IndexOf(row, key[j].ToString());
+
+                    if (indexKey != -1)
+                    {
+                        cipherText += VigenereAlphabet.AlphabetEn[indexKey];
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid string");
+                        Environment.Exit(0);
+                    }
+                    j++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid string");
+                Environment.Exit(0);
+            }
+
+
+            return cipherText;
+        }
+
+        private static string[] GetColumn(int row)
+        {
+            string[] row_res = new string[26];
+
+            string[,] matrix =  VigenereAlphabet.TabulaRectaEn;
+
+            for (int i = 0; i < 26; i++)
+            {
+                row_res[i] = matrix[i, row];
+            }
+
+            return row_res;
+        }
+
+        private static string[] GetRow(int row)
+        {
+            string[] row_res = new string[26];
+
+            string[,] matrix = VigenereAlphabet.TabulaRectaEn;
+
+            for (int i = 0; i < 26; i++)
+            {
+                row_res[i] = matrix[row, i];
+            }
+
+            return row_res;
+        }
+
+        private static int FindChar(string symbol)
+        {
+            return Array.IndexOf(VigenereAlphabet.AlphabetEn, symbol);
+        }
+    }
+}
--- KT/Program.cs
+++ KT/Program.cs
@@ -19,20 +19,20 @@ namespace KT
             string langEn = "en";
             string langRu = "ru";
 
-            /*Console.WriteLine("Task 1. Part 1.1. Route permutation ciphertext");
+            Console.WriteLine("Task 1. Part 1.1. Route permutation ciphertext");
             int n = 3;
             int m = 10;
             string cipherText = TabularRoutingCipher.Encrypting(originalText, n, m);
             string text = TabularRoutingCipher.Decrypting(cipherText, n, m);
             
-            Console.WriteLine($"Original text: {originalText}\nCipher text: {cipherText}\nDecrypt text: {text}");*/
+            Console.WriteLine($"Original text: {originalText}\nCipher text: {cipherText}\nDecrypt text: {text}");
 
-            /*Console.WriteLine("\nTask 1. Part 1.2. Playfair Ciphertext");
+            Console.WriteLine("\nTask 1. Part 1.2. Playfair Ciphertext");
 
             string cipherTextPlayfair = PlayfairCiphertext.EncryptDecrypt(originalText);
             string decryptText = PlayfairCiphertext.EncryptDecrypt(cipherTextPlayfair, false);
 
-            Console.WriteLine($"Original text: {originalText}\nCipher text: {cipherTextPlayfair}\nDecrypt text: {decryptText}");*/
+            Console.WriteLine($"Original text: {originalText}\nCipher text: {cipherTextPlayfair}\nDecrypt text: {decryptText}");
 
             Console.WriteLine("\nTask 2. Part 1.1. Vigeners cipher");
             string cipherTextEn = VigenereCipher.Encrypt(originTextEn, keyEn, langEn);
@@ -44,7 +44,11 @@ namespace KT
             Console.WriteLine($"Lang - En\nKey = {keyEn} Text = {originTextEn} Cipher = {cipherTextEn} Decrypt = {decryptEn}");
             Console.WriteLine($"Lang - Ru\nKey = {key} Text = {originalText} Cipher = {cipherTextRu} Decrypt = {decryptRu}");
 
-            //Console.WriteLine("\nTask 2. Part 1.2. Beaufort cipher");
+            Console.WriteLine("\nTask 2. Part 1.2. Beaufort cipher");
+            string strEncrypt  = BeaufortCipher.Encrypt(originTextEn, keyEn);
+            string strDecrypt = BeaufortCipher.Decrypt(strEncrypt, keyEn);
+
+            Console.WriteLine($"Key = {keyEn} Text = {originTextEn} Cipher = {strEncrypt} Decrypt = {strDecrypt}");
         }
     }
 }
